da762a0204f73096abc5422ac60b52881a326af2
Danny Mardini <dannymardini@gmail.com>
2020-01-11 21:41:23 -0800

    Adding a ScoreText prefab that is used to track a player's button presses.

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    // TODO: Remember to not use KeyCodes in the future.
+    public KeyCode updateKey;
+
+    private Text text;
+    private int numPresses = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(updateKey)) {
+            numPresses++;
+            text.text = numPresses.ToString();
+        }
+    }
+}
